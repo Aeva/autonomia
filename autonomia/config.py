1d8525ab39cf9489c0ac6db2809aaeef75d45c22
Aeva <aeva.ntsc@gmail.com>
2024-04-27 17:28:48 -0500

    restored 8 minutes 2 intervals config

--- autonomia/config.py
+++ autonomia/config.py
@@ -13,13 +13,13 @@ class Config:
 
         # An interval consists of an active BPM calibration phase, followed by steady state phase,
         # followed by a cooldown phase.
-        self.intervals = 1
+        self.intervals = 2
 
         # Number of minutes to find the intensity setpoint at the start of an interval.
         self.calibration_time = 2
 
         # Number of minutes to exercise at the calibrated setpoint.
-        self.steady_time = 10
+        self.steady_time = 8
 
         # Number of minutes to spend cooling down after an interval.
         self.cooldown_time = 1
