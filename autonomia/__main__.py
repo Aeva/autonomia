01aff95a036562eb71474b0a9b55708ed3b8c36f
Aeva <aeva.ntsc@gmail.com>
2024-06-09 00:13:54 -0500

    bluetooth hrm scanning

--- autonomia/__main__.py
+++ autonomia/__main__.py
@@ -6,6 +6,9 @@ warnings.simplefilter("ignore")
 import pygame
 warnings.resetwarnings()
 
+import asyncio
+from bleak import BleakScanner
+
 import sys
 import argparse
 import pathlib
@@ -13,6 +16,26 @@ from gui import Display
 from workout import workout_main, viewer_main
 from battery import battery_main
 from heart import bpm_debug_main
+
+
+def bluetooth_scan():
+    device_scan = {}
+
+    async def inner():
+        def callback(device, info):
+            if info.service_uuids.count("0000180d-0000-1000-8000-00805f9b34fb"):
+                address = device.address
+                if not device_scan.get(address):
+                    device_scan[address] = info.local_name
+                    print(f"\t{address} -> {info.local_name}")
+
+        scanner = BleakScanner(callback)
+        await scanner.start()
+        await asyncio.sleep(5)
+        await scanner.stop()
+
+    print("Searching for compatible bluetooth devices...\n")
+    asyncio.run(inner())
 
 
 if __name__ == "__main__":
@@ -53,10 +76,15 @@ if __name__ == "__main__":
         help="plot bpm on a global scale")
 
     parser.add_argument(
-        "--bluetooth",
+        "--bluetooth_address",
         action="store",
         type=str,
-        help="bluetooth hrm address")
+        help="preferred bluetooth device to connect to")
+
+    parser.add_argument(
+        "--bluetooth_scan",
+        action="store_true",
+        help="scan for compatible bluetooth devices")
 
     parser.add_argument(
         "--battery",
@@ -71,8 +99,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     device_addr = None
-    if args.bluetooth:
-        device_addr = args.bluetooth
+    if args.bluetooth_scan:
+        device_addr = bluetooth_scan()
+        sys.exit(0)
+
+    elif args.bluetooth_address:
+        device_addr = args.bluetooth_address
     else:
         device_addr = os.environ.get("BLE_HRM_ADDRESS")
 
